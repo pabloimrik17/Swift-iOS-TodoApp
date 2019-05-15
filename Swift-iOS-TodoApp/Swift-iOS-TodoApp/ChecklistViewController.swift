518d02773889f369867f7f864d6155914676ef12
Pablo Fernandez Guerra <pabloimrik17@gmail.com>
2019-05-15 23:33:49 +0200

    Removed basic View Controller, instead added a Table View Controller as main entry point.

--- Swift-iOS-TodoApp/Swift-iOS-TodoApp/ChecklistViewController.swift
+++ Swift-iOS-TodoApp/Swift-iOS-TodoApp/ChecklistViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class Checklist ViewController: UIViewController {
+class ChecklistViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
