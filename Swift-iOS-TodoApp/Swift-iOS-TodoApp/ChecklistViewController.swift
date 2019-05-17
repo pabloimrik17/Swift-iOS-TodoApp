96a4ebbb60a3fd5ae749924b57eef147916aa2be
Pablo Fernandez Guerra <pabloimrik17@gmail.com>
2019-05-17 19:39:09 +0200

    Added some row content (label cell and checkmark).

--- Swift-iOS-TodoApp/Swift-iOS-TodoApp/ChecklistViewController.swift
+++ Swift-iOS-TodoApp/Swift-iOS-TodoApp/ChecklistViewController.swift
@@ -14,6 +14,26 @@ class ChecklistViewController: UITableViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    // Devuelve el numero de filas de la tabla
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    // Establece el contenido con el que se visualiza cada columna de la fila.
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+        
+        if let label = cell.viewWithTag(1000) as? UILabel {
+            if indexPath.row == 0 {
+                label.text = "Run a maraton"
+            } else {
+                label.text = "Sleep"
+            }
+        }
+        
+        return cell
+    }
 
 
 }
