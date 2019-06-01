fec71aa0dc46c4cb043e222d2c84a020ca0cec52
Pablo Fernandez Guerra <pabloimrik17@gmail.com>
2019-06-01 02:22:59 +0200

    willSelectRow is overrided in order to ignore the row selection.
    Added outlets with the two button of the view.
    
    Now the add button goes back to the main view.
    
    Set the textField as the first responder on the view and resigns when the return button is pushed.
    
    Now the add button is disabled when no name is typed.

--- Swift-iOS-TodoApp/Swift-iOS-TodoApp/AddItemTableViewController.swift
+++ Swift-iOS-TodoApp/Swift-iOS-TodoApp/AddItemTableViewController.swift
@@ -12,17 +12,51 @@ class AddItemTableViewController: UITableViewController {
 
     @IBOutlet weak var textField: UITextField!
     
+    @IBOutlet weak var addButton: UIBarButtonItem!
+    @IBOutlet weak var cancelButton: UIBarButtonItem!
     @IBAction func cancel(_ sender: Any) {
         navigationController?.popViewController(animated: true)
     }
     
     @IBAction func add(_ sender: Any) {
-        
+        navigationController?.popViewController(animated: true) 
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         navigationItem.largeTitleDisplayMode = .never
+    }
+    
+    override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        return nil
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        textField.becomeFirstResponder()
+    }
+}
+
+extension AddItemTableViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return false
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        guard let oldText = textField.text,
+            let stringRange = Range(range, in: oldText) else {
+                return false
+        }
+        
+        let newText = oldText.replacingCharacters(in: stringRange, with: string)
+        
+        if newText.isEmpty {
+            addButton.isEnabled = false
+        } else {
+            addButton.isEnabled = true
+        }
+        
+        return true
     }
 }
